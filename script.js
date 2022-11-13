458e72ce239e44845d81de7282140188a3585128
Efren Jimenez <efrenjimenez_96@hotmail.com>
2022-11-13 01:38:30 -0600

    se implementó la redimensión del grid

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
+//Crea y redimenciona el grid
+function createGrid(dimension){ 
+    //Calcula cuántos elementos debe de haber por fila
+    elements = dimension**2
+    elementWidth = container.offsetWidth/dimension;
 
+    //Calcula cuántos elementos tiene actualmente el contenedor
+    childs = container.childElementCount;
+
+    //Si se requieren crear más cuadros
+    if (childs<elements) {
+        for (let i = childs; i<elements; i++){
+            const square = document.createElement('div');
+            square.classList.add('grid-item');
+            square.style.backgroundColor = '#ffffff';
+            container.appendChild(square);
+        }
+    //Si se requieren eliminar algunos cuadros
+    }else if (childs>elements){
+        for (let i = elements; i<childs; i++){
+            container.removeChild(container.lastElementChild);
+        }
+    }
+
+    //Redimensiona cómo se ve el grid
+    container.style.gridTemplateColumns = 'repeat('+dimension+',1fr)';
+    let gridItems = document.querySelectorAll('.grid-item');
+
+    //Dale la propiedad de hover
+    gridItems.forEach(element => element.addEventListener('mouseover',function () {this.style.backgroundColor = 'salmon'}))
+
+}
 const container = document.getElementById('container')
 let bgColor = '#ffffff';
 container.style.backgroundColor = bgColor;
 
-dimension = 15
-elements = dimension**2
+dimension = 15;
+createGrid(dimension);
+//template = 'repeat('+dimension+',1fr)';
 
-elementWidth = container.offsetWidth/dimension;
 
-//container.style.gridTemplateRows = "repeat(${dimension}, ${elementWidth}px)";
-
-for (let i = 0; i<elements; i++){
-    const square = document.createElement('div');
-    square.classList.add('grid-item');
-    if(i%2==0){
-        square.style.backgroundColor = '#ff1fff';
-    }else{
-        square.style.backgroundColor = '#AfA101';
-    }
-    container.appendChild(square);
-    square.classList.add('border-top-left')
+//function pixel(){
     
-}
-template = 'repeat('+dimension+',1fr)'
-container.style.gridTemplateColumns = template//'repeat(3,1fr)'
-let gridItems = document.querySelectorAll('.grid-item');
 
-function pixel(){
-    this.style.backgroundColor = 'salmon'
-}
+let boton = document.querySelector(".boton");
 
-gridItems.forEach(element => element.addEventListener('mouseover',pixel))
-
+boton.addEventListener("click",() => {
+    let dimension = prompt("Seleccione las dimensiones del recuadro");
+    createGrid(dimension);
+    });
